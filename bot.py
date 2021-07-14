71460273dfa94fe2d5252252fc018234ecc645fd
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-07-14 13:37:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ class Chat:
             file_name = Music.get_title(self, result) +' - @TLMusicDownloader_bot '+str(randint(0,999999))+'.mp3'
             file_name = file_name.replace('"', '')
 
-            self.send_message(['🎵 '+ Music.get_title(self, result) +'\n'+'🔗 ')])
+            self.send_message(['🎵 '+ Music.get_title(self, result) +'\n'+'🔗')])
             downloading_message = self.send_message('⬇️ İndiriliyor...')
 
             Music.download_music(self, file_name)
